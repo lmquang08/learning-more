fe289f6e72c17036d590605bbb6ecb98932f63a8
lmquang08 <lmquang0812@gmail.com>
2024-04-17 09:48:04 +0700

    test ci/cd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,7 @@
 @extends('layouts.master')
 @section('title', 'HomePage')
+@section('content')
+    <div class="container landing-page">
+        landing-page
+    </div>
+@stop
--- routes/user.php
+++ routes/user.php
@@ -3,6 +3,7 @@
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/register', function () {
